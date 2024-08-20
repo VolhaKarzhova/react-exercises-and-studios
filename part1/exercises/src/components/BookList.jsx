6f760cb384ceda1e90a909fd168c19835c0a2d10
Olga Karzhova <olgakorzhova@Olgas-MacBook-Air.local>
2024-08-19 19:57:10 -0500

    add part 1 and 2

--- part1/exercises/src/components/BookList.jsx
+++ part1/exercises/src/components/BookList.jsx
@@ -1,15 +1,15 @@
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+   let pageTitle = "My favorite books";
+   let book1 = "https://m.media-amazon.com/images/I/81oHM-dzefL._AC_UF1000,1000_QL80_.jpg";
+   let book2 = "https://m.media-amazon.com/images/I/71ZfyzgSdpL._AC_UF1000,1000_QL80_.jpg";
+   let book3 = "https://m.media-amazon.com/images/I/8123QX3n+SL._SY466_.jpg";
 
    return (
       <div>
          <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
+         <img src={book1} alt="War and Piece" />
+         <img src={book2} alt="The Master and Margarita" />
+         <img src={book3} alt="Pinocchio" />
       </div>      
    );
 }
--- part1/exercises/src/components/ChoresList.jsx
+++ part1/exercises/src/components/ChoresList.jsx
@@ -1,3 +1,13 @@
+import classes from './ChoresList.module.css';
+
 export default function ChoresList () {
-   return <p>No content is here!</p>;
+   const chores = ["Wash the cat", "Feed the cat", "Clean after cat"];
+   return (<p>
+             <h3 className = {classes.choresHeading}>My chores today:</h3>
+             <ul>
+                <li className={classes.choresText}>{chores[0]}</li>
+                <li className={classes.choresText}>{chores[1]}</li>
+                <li className={classes.choresText}>{chores[2]}</li>
+         </ul>
+      </p>);
 }
